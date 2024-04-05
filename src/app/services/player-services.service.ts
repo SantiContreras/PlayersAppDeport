d20d51a8f404534b15c7005967eee6f9f6dd5918
santi <santiagolasconi@gmail.com>
2024-04-05 12:17:54 -0300

    impelemntado metodos del servicio player

--- src/app/services/player-services.service.ts
+++ src/app/services/player-services.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable  } from 'rxjs';
+import { Jugador } from '../components/jugadores/jugador';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerServicesService {
+   
+  private urlEndPoing:string = 'http://localhost:8080/api/lista';
 
-  constructor() { }
+  constructor(private http : HttpClient) { 
+  }
+
+  getClientes():Observable<Jugador[]>{
+    return this.http.get<Jugador[]>(this.urlEndPoing)
+  
+  }
 }
